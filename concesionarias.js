6436e00b54932b5644622b51c617d289284e3fe4
unknown <you@example.com>
2022-04-06 15:06:00 -0300

    finalizando, quedando pendiente el ultimo

--- concesionarias.js
+++ concesionarias.js
@@ -1,4 +1,6 @@
 const autos = require ('./autos');
+let persona = require ('./personas');
+
 
 const concesionaria = {
     autos: autos,
@@ -30,22 +32,54 @@ const concesionaria = {
 
     
     listaDeVentas : function (){
-        if(autos.filter(row => row.vendido === true).length > 0){
-            let precioAutoVendido = autos.filter(row => row.precio);
-            return precioAutoVendido;
+        let autoVendido = this.autos.filter(row =>{
+            return row.vendido === true;
+        })
+        let ganancia = autoVendido.map(row => {
+            return row.precio;
+        })
+        return console.log(ganancia);
+    },
+
+    totalDeVentas : function (){
+        let listaTotal = this.listaDeVentas();
+        if(listaTotal.length === 0){
+           return 0;
         } else {
-            return null;    
+       let total = this.listaDeVentas().reduce(function(acum, valor){
+          return acum + valor;
+       })
+          return total;
+        }
+     },
+        
+     
+     puedeComprar : function (auto, persona) {
+        
+        let valorAuto = auto.precio;
+        let valorCuotas = valorAuto / auto.cuotas;
+        let totalCliente = persona.capacidadDePagoTotal;
+        let cuotasCliente = persona.capacidadDePagoEnCuotas;
+
+        if((valorAuto <= totalCliente) && (valorCuotas <= cuotasCliente)){
+            return true;
+        } else {
+            return false;
         }
     },
+
+    autosQuePuedeComprar : function (persona){
+        let autosParaVender = this.autosParaLaVenta();
+        let autosAptos = autosParaVender.filter( auto => {
+            if(this.puedeComprar(auto.patente, persona)){
+                return auto;
+            }
+        })
+        return autosAptos;
+        
         
     }
+    
+}
 
-
-
-
-
-concesionaria.venderAuto('APL123');
-console.log(concesionaria.listaDeVentas());
-
-
-        
+console.log(autosQuePuedeComprar(persona));
---
+++ personas.js
@@ -0,0 +1,7 @@
+let persona = {
+    nombre: 'Juan',
+    capacidadDePagoEnCuotas: 2000000,
+    capacidadDePagoTotal: 10000000
+    }
+
+    module.exports = persona;
